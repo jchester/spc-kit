a91e977cc1cb1baa6173d188359b46183bcaa552
Jacques Chester <github@jacques.chester.id.au>
2023-02-07 13:25:19 -0500

    Initial commit
    - Basic schema for systems, instruments, samples and measurements
    - Basic sample stats view
    - SPC magic constants for sample sizes up to 25
    - Example data from Table 6.1 of Introduction to Statistical Quality Control 8th ed

---
+++ sql/postgresql/00-drop-everything.sql
@@ -0,0 +1,1 @@
+drop schema spc cascade;
---
+++ sql/postgresql/90-inserters.sql
@@ -0,0 +1,39 @@
+create or replace function spc.bulk_insert_example_data_measurements(
+  p_instrument_name text,
+  p_series_period   tstzrange,
+  p_measurements    decimal[][]
+)
+  returns void
+as
+$$
+declare
+  v_instrument_queried_id bigint;
+  v_sample_period         tstzrange;
+  v_measurement_period    tstzrange;
+  v_sample_inserted_id    bigint;
+  v_sample                decimal[];
+  v_measurement           decimal;
+begin
+  select id from spc.instruments where name = p_instrument_name into v_instrument_queried_id;
+  select tstzrange(lower(p_series_period), lower(p_series_period) + interval '1 minute')
+  into v_sample_period;
+
+  foreach v_sample slice 1 in array p_measurements loop
+    insert into spc.samples (instrument_id, period)
+    values (v_instrument_queried_id, v_sample_period)
+    returning id into v_sample_inserted_id;
+
+    select tstzrange(lower(v_sample_period), lower(v_sample_period) + interval '1 second') into v_measurement_period;
+
+    foreach v_measurement in array v_sample loop
+      insert into spc.measurements(sample_id, period, measured_value)
+      values (v_sample_inserted_id, v_measurement_period, v_measurement);
+
+      select tstzrange(upper(v_measurement_period), upper(v_measurement_period) + interval '1 second')
+      into v_measurement_period;
+    end loop;
+
+    select tstzrange(upper(v_sample_period), upper(v_sample_period) + interval '1 minute') into v_sample_period;
+  end loop;
+end
+$$ language plpgsql;

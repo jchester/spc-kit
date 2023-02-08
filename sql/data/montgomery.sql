e92a36044fbf6214d0b8538ca5fd9a88fb2190e8
Jacques Chester <github@jacques.chester.id.au>
2023-02-08 11:45:00 -0500

    Use 'infinite' value for upper limit

--- sql/data/montgomery.sql
+++ sql/data/montgomery.sql
@@ -41,7 +41,7 @@ select spc.bulk_insert_example_data_measurements(
 select spc.bulk_insert_example_data_measurements(
                'Flow Width',
                'Table 6.2',
-               '[2023-01-02 00:00:01,2023-01-03 00:00:00)'::tstzrange,
+               '[2023-01-02 00:00:01,)'::tstzrange,
                'limit_application',
                array [
                  array [1.4483,1.5458,1.4538,1.4303,1.6206],

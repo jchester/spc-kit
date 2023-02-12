8342d899c8c0dd810e036a7aadb30626280cdb07
Jacques Chester <github@jacques.chester.id.au>
2023-02-12 16:23:49 -0500

    Add p chart rules views

--- sql/postgresql/03-spc-reports-schema.sql
+++ sql/postgresql/03-spc-reports-schema.sql
@@ -102,3 +102,53 @@ This view applies the limits derived in s_limits to matching control windows, sh
 in-control and out-of-control according the s̄ limits on s. These signals are more effective than r_rules when sample
 size > 10.
 $$;
+
+create view spc_reports.p_conformant_rules as
+  select ss.sample_id
+       , control_w.id as control_window_id
+       , limits_w.id  as limit_establishment_window_id
+       , ss.period
+       , case
+           when mean_fraction_conforming > upper_control_limit then 'out_of_control_upper'
+           when mean_fraction_conforming < lower_control_limit then 'out_of_control_lower'
+           else 'in_control'
+         end          as p_control_status
+  from spc_intermediates.fraction_conforming_sample_statistics ss
+       join spc_data.windows                                   control_w on ss.period <@ control_w.period
+       join spc_data.window_relationships                      wr on control_w.id = wr.control_window_id
+       join spc_data.windows                                   limits_w
+            on limits_w.id = wr.limit_establishment_window_id
+       join spc_intermediates.p_limits_conformant on limits_w.id = p_limits_conformant.limit_establishment_window_id
+  where include_in_limit_calculations;
+
+comment on view spc_reports.p_conformant_rules is $$
+This view applies the limits derived in p_limits_conformant to matching control windows, showing which sample fractions
+conforming where in-control and out-of-control according to the limits on the fraction conforming.
+
+This is a non-traditional application, the typical approach is to set rules on fraction non-conforming. This is included
+for completeness.
+$$;
+
+create view spc_reports.p_non_conformant_rules as
+  select ss.sample_id
+       , control_w.id as control_window_id
+       , limits_w.id  as limit_establishment_window_id
+       , ss.period
+       , case
+           when mean_fraction_non_conforming > upper_control_limit then 'out_of_control_upper'
+           when mean_fraction_non_conforming < lower_control_limit then 'out_of_control_lower'
+           else 'in_control'
+         end          as p_control_status
+  from spc_intermediates.fraction_conforming_sample_statistics ss
+       join spc_data.windows                                   control_w on ss.period <@ control_w.period
+       join spc_data.window_relationships                      wr on control_w.id = wr.control_window_id
+       join spc_data.windows                                   limits_w
+            on limits_w.id = wr.limit_establishment_window_id
+       join spc_intermediates.p_limits_non_conformant
+            on limits_w.id = p_limits_non_conformant.limit_establishment_window_id
+  where include_in_limit_calculations;
+
+comment on view spc_reports.p_non_conformant_rules is $$
+This view applies the limits derived in p_limits_non_conformant to matching control windows, showing which sample
+fractions non-conforming where in-control and out-of-control according to the limits on the fraction non-conforming.
+$$;

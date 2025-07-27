5d4a5273cfd526d13bb43aef94f0dcd91a69d38f
Jacques Chester <github@jacques.chester.id.au>
2025-07-26 17:50:07 -0700

    Make drop more tolerant of fresh DBs

--- sql/00-drop-everything.sql
+++ sql/00-drop-everything.sql
@@ -1,5 +1,5 @@
 -- Copyright (C) 2024 Jacques Chester. See LICENSE.
 
-drop schema spc_reports cascade;
-drop schema spc_intermediates cascade;
-drop schema spc_data cascade;
+drop schema if exists spc_reports cascade;
+drop schema if exists spc_intermediates cascade;
+drop schema if exists spc_data cascade;
